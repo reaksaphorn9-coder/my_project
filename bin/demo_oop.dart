2fe60d9c8c52ce8bd6c638b7309d7dc34e67a938
unknown <reaksaphorn9@gmail.com>
2026-08-08 15:14:58 +0700

    Initial commit

---
+++ bin/demo_oop.dart
@@ -0,0 +1,5 @@
+import 'package:demo_oop/demo_oop.dart' as demo_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${demo_oop.calculate()}!');
+}
---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+void main(){
+  // Output
+  print("Hello Dart language");
+  // String dart
+  String teacherName = "Saren";
+  String studentName = 'Sok';
+  print("Teacher name's ${teacherName}");
+  print("Student name's "+studentName);
+  // Variable dart
+  int age = -34;
+  double salary = 100.90;
+  num totalSalary = 23.899;
+  bool isMarried = false;
+  isMarried = true;
+  // List
+  List<String> teacherNameList = [
+    "Saren","Sarin","Mich"
+  ];
+  Set<String> studentNameList = {"Sok","Dara"};
+  Map<String, String> loginRequest ={
+    "username":"admin",
+    "password":"1234"
+  };
+}
