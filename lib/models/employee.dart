6113e1af93ab2fad9f6423ad4a81b290714a5c07
ASUS <reaksaphorn@gmail.com>
2026-08-09 16:25:48 +0700

    feat: change name

---
+++ lib/models/employee.dart
@@ -0,0 +1,27 @@
+import 'package:demo_oop/models/person.dart';
+import 'package:demo_oop/models/student.dart';
+
+class Employee extends Person{
+  double? salary;
+  @override
+  void showInfo() {
+    super.showInfo();
+    print('Salary ${salary}');
+  }
+}
+
+void main(){
+  Employee employee = new Employee();
+  employee.id = 1;
+  employee.gender = "Male";
+  employee.name = "Metha";
+  employee.salary = 250;
+  employee.showInfo();
+  print("--------------Student---------");
+  Student student = new Student();
+  student.id = 23;
+  student.gender = "Female";
+  student.name = "Sot";
+  student.showInfo();
+
+}
