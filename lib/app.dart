1959cf95b30ef39da41b3891c2453659e17e4988
ASUS <reaksaphorn@gmail.com>
2026-08-11 18:09:24 +0700

    feat: change name

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,11 @@
-void main(){
+void main() {
   // Output
   print("Hello Dart language");
   // String dart
-  String teacherName = "Sar123n";
+  String teacherName = "Saren";
   String studentName = 'Sok';
   print("Teacher name's ${teacherName}");
-  print("Student name's "+studentName);
+  print("Student name's " + studentName);
   // Variable dart
   int age = -34;
   double salary = 100.90;
@@ -13,12 +13,25 @@ void main(){
   bool isMarried = false;
   isMarried = true;
   // List
-  List<String> teacherNameList = [
-    "Saren","Sarin","Mich"
-  ];
-  Set<String> studentNameList = {"Sok","Dara"};
-  Map<String, String> loginRequest ={
-    "username":"admin",
-    "password":"1234"
-  };
+  List<String> teacherNameList = ["Saren", "Sarin", "Mich"];
+  Set<String> studentNameList = {"Sok", "Dara"};
+  Map<String, String> loginRequest = {"username": "admin", "password": "1234"};
+  // Control Statement
+  if (age == 20) {}
+  // Switch case
+  int number = 10;
+  switch (number) {
+    case 5:
+      print("Number is 5");
+      break;
+    case 10:
+      print("Number is 10");
+      break;
+    default:
+      print("Number is $number");
+  }
+  // Loop
+  for(String teacher in teacherNameList){
+    print("Teacher name is ${teacher}");
+  }
 }
---
+++ lib/demo_funtion.dart
@@ -0,0 +1,27 @@
+// No return
+void showMessage(){
+
+}
+// Function have return type
+String getTeacherName(){
+  return "Saren";
+}
+// Function have  parameter
+void setName(String name){}
+// Arrow Function =>
+int getAge()=> 23;
+void register({String? firstName, String? lastName, String? gender, int? age}){
+  print("Register function");
+}
+void login(String username, String password,{ bool? member}){
+  print("Login function");
+}
+
+void main(){
+  print(getAge());
+  setName("Sok");
+  // call function
+  register(age: 12, gender: "Male");
+  // call function
+  login("admin","123", member: true);
+}
